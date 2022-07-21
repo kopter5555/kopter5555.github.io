e76c342e4b82db16cef7153e810cfa257425b957
kopter5555 <109735748+kopter5555@users.noreply.github.com>
2022-07-21 23:47:52 +0900

    最終課題

--- main.js
+++ main.js
@@ -3,3 +3,7 @@ alert('Hello!');
 let message;
 message='Hello!';
 console.log(message);
+function OnButtonClick(){
+    target=document.getElementById("output");
+    target.innerHTML="リオネル・メッシ、クリスティアーノ・ロナウド、ケヴィン・デブライネ";
+}
