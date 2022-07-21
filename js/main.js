35729b508eed3cba90678185c2a591adfb694125
kopter5555 <109735748+kopter5555@users.noreply.github.com>
2022-07-22 00:24:35 +0900

    最終課題

---
+++ js/main.js
@@ -0,0 +1,9 @@
+console.log('soccer');
+alert('Hello!');
+let message;
+message='Hello!';
+console.log(message);
+function OnButtonClick(){
+    target=document.getElementById("output");
+    target.innerHTML="リオネル・メッシ、クリスティアーノ・ロナウド、ケヴィン・デブライネ";
+}
